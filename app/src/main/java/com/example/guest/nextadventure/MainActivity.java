663d8c17f4f3fd5bec224dfb01ea664b2b19c970
Caleb Stevenson and Yusuf Qedan <caleb+yusuf@>
2016-11-17 10:51:19 -0800

    add hardcoded array to populate gridview

--- app/src/main/java/com/example/guest/nextadventure/MainActivity.java
+++ app/src/main/java/com/example/guest/nextadventure/MainActivity.java
@@ -2,15 +2,40 @@ package com.example.guest.nextadventure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.gridView) GridView mGridView;
+    private String[] categories = new String[] {"Men", "Women", "Paddle", "Camp", "Downhill Ski", "Snowboard", "Climb", "XC Ski", "Snowshoe"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Log.d("blah",categories[0]);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, categories);
+        mGridView.setAdapter(adapter);
 
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String category = ((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this, category, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
